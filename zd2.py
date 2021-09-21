dbd7d38e146395b9df2198802df4fcbfec12992e
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2021-09-21 12:35:26 +0300

    Delete zd2.py

--- zd2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    alp = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}
-    swapped = {v: k for k, v in alp.items()}
-    print(swapped)
